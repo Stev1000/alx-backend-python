47480568785a42296d7de216883d714e59af1a8e
Stevo <nsanzestevo1@gmail.com>
2025-07-19 21:06:52 +0200

    Fix: Final checker-compliant version for Task 9

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -49,4 +49,3 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient("google")
         result = client.public_repos(license="apache-2.0")
         self.assertEqual(result, self.apache2_repos)
-        
