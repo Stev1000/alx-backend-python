d0ffe5626f4d2fbd7ef0d297f79ed3e89e977f9d
Stevo <nsanzestevo1@gmail.com>
2025-07-20 14:50:49 +0200

    Fix nested serialization with messages in conversations

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,25 +1,46 @@
 from rest_framework import serializers
 from .models import User, Conversation, Message
 
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['user_id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'role', 'created_at']
-
-
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserSerializer(read_only=True)
+    sender = serializers.CharField(source='sender.username', read_only=True)
 
     class Meta:
         model = Message
-        fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at']
-
+        fields = ['message_id', 'sender', 'message_body', 'sent_at']
 
 class ConversationSerializer(serializers.ModelSerializer):
-    participants = UserSerializer(many=True, read_only=True)
-    messages = MessageSerializer(many=True, read_only=True, source='messages')
+    participants = serializers.StringRelatedField(many=True)
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'messages', 'created_at']
+
+    def get_messages(self, obj):
+        messages = obj.messages.all()
+        return MessageSerializer(messages, many=True).data
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['user_id', 'username', 'first_name', 'last_name', 'email', 'password', 'phone_number', 'role', 'created_at']
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            phone_number=validated_data.get('phone_number'),
+            role=validated_data.get('role')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def validate_role(self, value):
+        if value not in ['guest', 'host', 'admin']:
+            raise serializers.ValidationError("Invalid role")
+        return value
