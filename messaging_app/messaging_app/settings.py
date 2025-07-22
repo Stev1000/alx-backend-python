3a5abfa411511b772ab9c1e35475e9ddb1fd7b12
Stevo <nsanzestevo1@gmail.com>
2025-07-22 15:41:45 +0200

    Task 2: Add pagination and filtering to MessageViewSet

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -52,6 +52,12 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',   
         'rest_framework.authentication.SessionAuthentication', 
     ],
+    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter'
+    ]
 }
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
