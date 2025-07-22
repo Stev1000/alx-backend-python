a4929a2945520acba839a0a1e5df26d7987b43b6
Stevo <nsanzestevo1@gmail.com>
2025-07-22 15:51:52 +0200

    Fix: Add get_paginated_response with paginator.count to MessagePagination

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -2,10 +2,10 @@ import django_filters
 from .models import Message
 
 class MessageFilter(django_filters.FilterSet):
-    sent_after = django_filters.DateTimeFilter(field_name="sent_at", lookup_expr='gte')
-    sent_before = django_filters.DateTimeFilter(field_name="sent_at", lookup_expr='lte')
-    conversation_id = django_filters.NumberFilter(field_name="conversation__id")
+    start_time = django_filters.DateTimeFilter(field_name="sent_at", lookup_expr='gte')
+    end_time = django_filters.DateTimeFilter(field_name="sent_at", lookup_expr='lte')
+    sender = django_filters.CharFilter(field_name='sender__username', lookup_expr='icontains')
 
     class Meta:
         model = Message
-        fields = ['conversation_id', 'sent_after', 'sent_before']
+        fields = ['start_time', 'end_time', 'sender']
