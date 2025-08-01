c78298f3d93590aa4d99e3077dce564f5ba21d6e
Stevo <nsanzestevo1@gmail.com>
2025-08-01 11:50:58 +0200

    Task 3: Optimize threaded message retrieval using select_related and recursive logic

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('delete_user/', views.delete_user, name='delete_user'),
     path('account_deleted/', views.account_deleted, name='account_deleted'),
+    path('threaded/', views.threaded_conversations, name='threaded_conversations'),
+
 
 ]
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Message
+from django.db.models import Prefetch
 
 User = get_user_model()
 
@@ -15,3 +18,27 @@ def delete_user(request):
 
 def account_deleted(request):
     return HttpResponse("Your account and related data have been deleted.")
+
+@login_required
+def threaded_conversations(request):
+    messages = Message.objects.filter(receiver=request.user, parent_message__isnull=True)\
+        .select_related('sender', 'receiver')\
+        .prefetch_related(
+            Prefetch('replies', queryset=Message.objects.select_related('sender', 'receiver'))
+        )
+    
+    context = {
+        'messages': messages
+    }
+    return render(request, 'messaging/threaded_conversations.html', context)
+def get_replies_recursive(message):
+    replies_data = []
+    for reply in message.replies.all():
+        replies_data.append({
+            'id': reply.id,
+            'sender': reply.sender.username,
+            'content': reply.content,
+            'timestamp': reply.timestamp,
+            'replies': get_replies_recursive(reply)
+        })
+    return replies_data
