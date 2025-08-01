9c2296f2fe11fcaffc650135eb25b8c422824b6b
Stevo <nsanzestevo1@gmail.com>
2025-08-01 10:58:16 +0200

    Add message edit tracking using pre_save signal and MessageHistory model

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification,MessageHistory
+from django.db.models.signals import pre_save
+from django.utils.timezone import now
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
@@ -9,3 +11,18 @@ def create_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:  # Only for existing messages
+        try:
+            original = Message.objects.get(pk=instance.pk)
+            if original.content != instance.content:
+                MessageHistory.objects.create(
+                    message=original,
+                    old_content=original.content,
+                    edited_at=now()
+                )
+                instance.edited = True  # Mark as edited
+        except Message.DoesNotExist:
+            pass
